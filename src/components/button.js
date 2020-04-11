539b6b5cad9afc17fed10804f34ba2fd963c9f2f
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-04-11 16:38:38 +0300

    вынесла все константы в константы, завела файлы моков

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,7 @@
+import {BUTTON_MORE} from './constants.js';
+
 export const makeLoadButton = () => {
   return (
-    `<button class="load-more" type="button">load more</button>`
+    `<button class="load-more" type="button">${BUTTON_MORE}</button>`
   );
 };
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,1 +1,8 @@
 export const TASK_COUNT = 3;
+export const BUTTON_MORE = `load more`;
+export const SORT_DEF = `SORT BY DEFAULT`;
+export const SORT_DATE_D = `SORT BY DATE down`;
+export const SORT_DATE_U = `SORT BY DATE up`;
+export const MENU_ADD = `+ ADD NEW TASK`;
+export const MENU_T = `TASKS`;
+export const MENU_S = `STATISTICS`;
