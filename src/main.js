8f6b0acef0d79f5d0874d90c8fa7b8e49448db40
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-04-16 00:04:47 +0300

    фильтры, мелкие правки

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import {TASK_COUNT, TASKS_COUNT_ON_START, TASKS_COUNT_BY_BUTTON} from './components/constants.js';
 import {render} from './components/render.js';
 import {makeLoadButton} from './components/button.js';
-// import {makeTask, makeBoard} from './components/task.js';
 import {makeForm} from './components/form.js';
 import {makeSort} from './components/sort.js';
 import {makeMenu} from './components/menu.js';
 
 import {makeFiltersMarkup} from './components/filters.js';
-import {generateFilters} from './mock/filters.js';
+import {generateFilters, countTasksByType} from './mock/filters.js';
 
 import {generateTask, generateTasks, DefaultRepeatingDays} from "./mock/task.js";
 import {makeTask, makeBoard} from "./components/task.js";
@@ -28,26 +27,46 @@ render(boardContainer, sort, `afterbegin`);
 const boardTasks = document.querySelector(`.board__tasks`);
 
 const tasksData = generateTask();
-const tasks = generateTasks(TASK_COUNT);
-render(boardTasks, makeTask(tasks[0]), `beforeend`);
+// const tasks = generateTasks(TASKS_COUNT_ON_START - 1);
+const tasks = generateTasks(TASK_COUNT - 1);
+render(boardTasks, makeTask(tasks[0]), `afterbegin`);
 render(boardTasks, form, `afterbegin`);
 
-const filters = generateFilters();
+const all = tasks.length;
+const over = 0;
+const today = 0;
+const rep = 0;
+const fav = countTasksByType(tasks, `isFavourite`);
+const arch = countTasksByType(tasks, `isArchive`);
+const filterCounts = [all, over, today, fav, rep, arch];
+console.log(filterCounts);
+
+const filters = generateFilters(tasks, filterCounts);
 const filtersMarkup = makeFiltersMarkup(filters);
 render(menuContainer, filtersMarkup, `afterend`);
-
-render(boardContainer, button, `beforeend`);
-const buttonMore = document.querySelector(`.load-more`);
 
 let showingTasksCount = TASKS_COUNT_ON_START;
 tasks.slice(1, showingTasksCount)
   .forEach((task) => render(boardTasks, makeTask(task), `beforeend`));
+
+if (tasks.length >= TASKS_COUNT_ON_START) {
+  render(boardContainer, button, `beforeend`);
+}
+
+const buttonMore = document.querySelector(`.load-more`);
+if (buttonMore) {
+
 buttonMore.addEventListener(`click`, () => {
-  const prevTasksCount = showingTasksCount;
-  showingTasksCount = showingTasksCount + TASKS_COUNT_BY_BUTTON;
-  tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(boardTasks, makeTask(task), `beforeend`));
-  if (showingTasksCount >= tasks.length) {
-    buttonMore.remove();
-  }
-});
+
+    const prevTasksCount = showingTasksCount;
+    showingTasksCount = showingTasksCount + TASKS_COUNT_BY_BUTTON;
+    tasks.slice(prevTasksCount, showingTasksCount)
+      .forEach((task) => render(boardTasks, makeTask(task), `beforeend`));
+
+    if (showingTasksCount >= tasks.length) {
+      buttonMore.remove();
+    }
+
+  });
+
+}
--- src/mock/filters.js
+++ src/mock/filters.js
@@ -1,10 +1,26 @@
-const filterNames = [`all`, `overdue`, `today`, `favorites`, `repeating`, `archive`]
+const filterNames = [`all`, `overdue`, `today`, `favorites`, `repeating`, `archive`];
 
-export const generateFilters = () => {
-  return filterNames.map((it) => {
+export const countTasksByType = (tasksArray, filterName) => {
+  const filtered = tasksArray.filter((item) => !!item.filterName);
+  return filtered.length;
+};
+
+export const generateFilters = (tasksArray, counts) => {
+  let result = [];
+  for (let i = 0; i < filterNames.length; i += 1) {
+    let item = {
+      title: filterNames[i],
+      count: counts[i]
+    };
+    result.push(item);
+  }
+  return result;
+  /*
+  return filterNames.map((item) => {
     return {
-      title: it,
+      title: item,
       count: Math.floor(Math.random() * 10),
     };
   });
-}
+*/
+};
